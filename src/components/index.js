22e8cc051137482e64414efee8036b439eb6f2f6
Rafael 99 <chipalino2009@list.ru>
2020-11-11 00:58:26 +0300

    Added pizzablock in home/pages

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Header from './Header';
 import Categories from './Categories';
 import Sortpopup from './Sortpopup';
 import Button from './Button';
+import PizzaBlock from './PizzaBlock';
 
-export { Header, Categories, Sortpopup, Button };
+export { Header, Categories, Sortpopup, Button, PizzaBlock };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './scss/app.scss';
 import App from './App';
+import { createStore } from 'redux';
+
+function counterReducer(state = { value: 0 }, action) {
+  switch (action.type) {
+    case 'counter/incremented':
+      return { value: state.value + 1 };
+    case 'counter/decremented':
+      return { value: state.value - 1 };
+    default:
+      return state;
+  }
+}
+let store = createStore(counterReducer);
 
 ReactDOM.render(
   <React.StrictMode>
